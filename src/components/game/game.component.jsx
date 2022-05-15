1346fd69e3319880893433a51a8804f6f7431e8a
Fernando <catOWO2011@gmail.com>
2022-05-15 15:51:17 -0400

    TIC-7: Added Player list component and functinality to select two players

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -1,17 +1,21 @@
 import { Component } from "react";
 
 import { Board } from "../board/board.component";
-import { Player } from "../player/player.component";
+import { PlayerList } from "../player-list/player-list.component";
 
 import './game.styles.css';
 
 export class Game extends Component {
   state = {
     board: Array(9).fill(null),
-    players: []
+    players: [],
+    showPlayerList: true,
+    showBoard: false,
+    firstPlayer: null,
+    secondPlayer: null,
   }
 
-  handleClickOnCell = ({target}) => {
+  handleClickOnCell = ({ target }) => {
     const cellId = parseInt(target.id, 10);
     const board = this.state.board.slice();
     if (board[cellId] === null) {
@@ -20,37 +24,71 @@ export class Game extends Component {
     }
   }
 
+  loaderAsyncCall() {
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
+
+  handleClickOnPlayer = ({ target }) => {
+    const cellId = parseInt(target.id, 10);
+
+    const { firstPlayer, secondPlayer, players } = this.state;
+    
+    if (firstPlayer === null) {
+      this.setState( { firstPlayer: players[cellId - 1] } );
+    } else {
+      if (secondPlayer === null) {
+        this.setState( {
+          secondPlayer: players[cellId - 1]
+        } );
+        this.loaderAsyncCall().then(() => {
+          this.setState({
+            showPlayerList: false,
+            showBoard: true
+          });
+        });
+      }
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) {
+        throw Error(response.statusText);
+      } else {
+        const players = await response.json();
+        this.setState({ players: [].concat(players) });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   render() {
+    const { board, players, showPlayerList, showBoard, firstPlayer, secondPlayer } = this.state;
+
     return (
-      <div>
-        <div
-          className="ttt-player-list-container"
-        >
-          <ul className="ttt-player-list">
-            <li>
-              
-            </li>
-            <li>
-              
-            </li>
-          </ul>
-        </div>
-        <div
-          className="ttt-game-board"
-        >
-          <Player
-            name="Bob"
-            imagePath={'https://robohash.org/3?set=set4'}
-          />
-          <Board
-            board={this.state.board}
-            handleClickOnCell={this.handleClickOnCell}
-          />
-          <Player
-            name="Alice"
-            imagePath={'https://robohash.org/2?set=set4'}
-          />
-        </div>
+      <div className="game-container">
+        {
+          showPlayerList === true?
+            <PlayerList
+              players={players}
+              handleClickOnPlayer={this.handleClickOnPlayer}
+              firstPlayer={ firstPlayer }
+              secondPlayer={ secondPlayer }
+            />
+          : null
+        }
+        {
+          showBoard === true ?
+            <Board
+              board={board}
+              firstPlayer={ firstPlayer }
+              secondPlayer={ secondPlayer }
+              handleClickOnCell={this.handleClickOnCell}
+            />
+           : null
+        }
       </div>
     );
   }
